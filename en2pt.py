cb27ace7f999198e5c283f543f5a6818f260a385
Ronald Kaiser <raios.catodicos@gmail.com>
2011-12-02 00:42:32 -0200

    fist version, wip

---
+++ en2pt.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#
+# File: en2pt.py
+# Description: translate a word from english to portuguese
+# Usage: 
+#       $ ./en2pt.py word
+#
+# TODO: treat bold tag
+# TODO: treat bullets
+
+
+import re
+import sys
+import urllib2
+import HTMLParser
+
+#searching
+url = 'http://michaelis.uol.com.br/moderno/ingles/index.php?lingua=ingles-portugues&palavra='
+word = sys.argv[1]
+r = urllib2.urlopen('%s%s' % (url, word))
+html = r.read()
+
+#parsing
+pars = HTMLParser.HTMLParser()
+pattern = r"<span class='descricao'>(.*)</span>"
+d = re.search(pattern, html).groups()[0]
+d = pars.unescape(d)
+d = re.split('<B> *\d+</B>', d)[1:]
+meanings = [line.strip() for line in d]
+
+for i, meaning in enumerate(meanings):
+    print "%s - %s" % (i+1, meaning)
